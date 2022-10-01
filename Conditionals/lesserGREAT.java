0d08283b44c3a2f2ab644eee009c005c01892fda
J Gilles <jgilles123@gmail.com>
2022-10-01 15:09:26 -0400

    Conditional IF examples created.

---
+++ Conditionals/lesserGREAT.java
@@ -0,0 +1,32 @@
+/*  The lesserGREAT conditional will compare two values
+ *  using <, >
+ *
+ *  Oct. 1, 2022
+ * */
+
+class lesserGREAT{
+    
+    public static void main(String[] args){
+        int elephant = 2800;
+        int cat = 17;
+        
+        // The greater than operator >
+        if (elephant > cat){
+            System.out.println("The elephant weigh more than the cat.");
+
+        }
+        // The less than operator <
+        if (elephant < cat){
+            System.out.println("The cat weigh less than the elephant.");
+
+        }
+        // The is_equal comparitor ==
+        if (elephant == cat){
+            System.out.println("The elephant and the car are not equal in weight.");
+
+        }
+        
+    }
+
+
+}
